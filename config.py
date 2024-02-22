6821f373a299eb45f76fea24e1f9bbede05aa81e
Akopian1 <148681394+Akopian1@users.noreply.github.com>
2024-02-22 14:01:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-
+TOKEN = '6879019359:AAEO5qaSturJhik5Aov11Z9THuYD2dHju68'
+keys = {
+    'Биткоин' : 'BTC',
+    'Драм' : 'AMD',
+    'Евро' : 'EUR',
+    'Доллар' : 'USD',
+    'Рубль' : 'RUB',
+}
